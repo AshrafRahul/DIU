5195f4a68f87f03c0dc712f89d6e1af69bfc1037
Md Ashraful Islam <m.ashraful.islam007@gmail.com>
2024-12-31 00:51:51 +0600

    update

--- SE321_Software_Engineering_Web-Application/Problem/15_three_sum.php
+++ SE321_Software_Engineering_Web-Application/Problem/15_three_sum.php
@@ -1,31 +1,32 @@
 <?php
-class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        set<vector<int>>v;
-        vector<int>v2;
-        for(int i=0;i<nums.size();i++){
-            int j=i+1,k=nums.size()-1;
-            long long int s;
-            while(j<k){
-                 s=nums[i]+nums[j]+nums[k];
-                 if(s==0)
-                 {
-                 v.insert({nums[i],nums[j],nums[k]});
-                 j++,k--;
-            }else if(s<0){
-                j++;
-            }else{
-                k--;
-            }
-            }
+
+function threeSum($nums) {
+    $n = count($nums);
+    $result = [];
+
+    if ($n < 3) return $result;
+
+    sort($nums);
+
+    for ($i = 0; $i < $n - 2; $i++) {
+        if ($i > 0 && $nums[$i] == $nums[$i - 1]) continue;
+
+        $l = $i + 1;
+        $r = $n - 1;
+
+        while ($l < $r) {
+            $sum = $nums[$i] + $nums[$l] + $nums[$r];
+
+            if ($sum == 0) {
+                $result[] = [$nums[$i], $nums[$l], $nums[$r]];
+                while ($l < $r && $nums[$l] == $nums[$l + 1]) $l++;
+                while ($l < $r && $nums[$r] == $nums[$r - 1]) $r--;
+                $l++;
+                $r--;
+            } elseif ($sum < 0) $l++;
+            else $r--;
         }
-        vector<vector<int>>f;
-        for(auto i:v){
-            f.push_back(i);
-        }
-      return f;
     }
-};
-?>
+
+    return $result;
+}
